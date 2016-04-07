2cfa4077486347e18bf3709ed5f78a43ff246da9
Zinoq <0893504@hr.nl>
2016-04-07 13:57:24 +0200

    Database is semi working

--- src/Database.java
+++ src/Database.java
@@ -6,12 +6,22 @@ import java.sql.*;
 
 
 public class Database {
-
     public static void main(String[] args) {
-
         try {
             // connectie met database maken
-            Connection connection1 = DriverManager.getConnection("jdbc:mysql://localhost/parkeerautomaten1?"+"user=root&password=Kappa123");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/parkeerautomaten1?"+"user=root&password=Kappa123");
+
+            //Statement maken
+            Statement myStatement = connection.createStatement();
+
+            // Alles binnen de databases selecteren
+            String sqlCommand = "SELECT * FROM markten";
+            ResultSet myResultset = myStatement.executeQuery(sqlCommand);
+
+            //Process output
+            while(myResultset.next()) { //loop door alle resultaten
+                System.out.println(myResultset.getString("straatnaam"));
+            }
         }
         catch (Exception a) {
             a.printStackTrace();
@@ -20,3 +30,4 @@ public class Database {
     }
 }
 
+//TODO make it so that we can execute sql commands from outside of the main here
