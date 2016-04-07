1ad4ce9e27c8565a5a336f0e7c5119ebe54bf539
Zinoq <0893504@hr.nl>
2016-04-07 10:48:28 +0200

    edited

--- src/Database.java
+++ src/Database.java
@@ -5,11 +5,28 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Database {
-    String url = "jdbc:postgresql://localhost:5432/public";
-    String username = "postgres";
-    String password = "Kappa123";
-    Class.forName("org.postgresql.Driver");
+    String url;
+    String username;
+    String password;
+    Connection db;
 
-    Connection db = DriverManager.getConnection(url, username, password);
-
+    public Database() {
+        String url = "jdbc:postgresql://localhost:5432/public";
+        String username = "postgres";
+        String password = "Kappa123";
+        try {
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Succes");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+
+        try {
+            Connection db=  DriverManager.getConnection(url, username, password);
+            System.out.println("Succes");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
