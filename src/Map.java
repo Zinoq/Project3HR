25dcc765b56411c14ab3b4faf9394dd4467111e8
AndreHummel <andre.97@hotmail.com>
2016-04-11 14:45:04 +0200

    kaart database connectie

--- src/Map.java
+++ src/Map.java
@@ -8,31 +8,27 @@ import com.lynden.gmapsfx.javascript.object.Marker;
 import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
-
 
 public class Map extends Application implements MapComponentInitializedListener{
         GoogleMapView mapView;
         GoogleMap map;
-        static Map Zino;
+        Database Database;
 
-        public Map(Stage stage) throws Exception {
+        public Map(){
+            //Create the JavaFX component and set this as a listener so we know when
+            //the map has been initialized, at which point we can then begin manipulating it.
             mapView = new GoogleMapView();
             mapView.addMapInializedListener(this);
+            Database = new Database();
+        }
 
-            Scene scene = new Scene(mapView);
-
+        @Override
+        public void start(Stage stage) throws Exception {
+            Scene scene = new Scene(mapView);;
             stage.setTitle("Map");
             stage.setScene(scene);
             stage.show();
-        }
-        @Override
-        public void start(Stage stage) throws Exception {
-            Map Zino = new Map(stage);
-            //Create the JavaFX component and set this as a listener so we know when
-            //the map has been initialized, at which point we can then begin manipulating it.
-;
         }
 
         @Override
@@ -52,38 +48,29 @@ public class Map extends Application implements MapComponentInitializedListener{
 
             map = mapView.createMap(mapOptions);
 
-            //Add a marker to the map.
-//            MarkerOptions markerOptions = new MarkerOptions();
-//
-//            markerOptions.position( new LatLong(51.917377, 4.483920) )
-//                    .visible(Boolean.TRUE)
-//                    .title("School");
-//
-//            Marker marker = new Marker( markerOptions );
-//
-//            map.addMarker(marker);
-//
-        }
 
-        public void createMarker(double Lat, double Long, Boolean visibility, String title) { // probleem = dat er geen object gemaakt wordt en dus kunnen we dit niet uitvoeren.
+            //Add a marker to the map.
             MarkerOptions markerOptions = new MarkerOptions();
 
-            markerOptions.position( new LatLong(Lat, Long) )
-                    .visible(visibility)
-                    .title(title);
-            Marker marker = new Marker( markerOptions );
-            map.addMarker(marker);
+            for (int i = 0; i < Database.getMarkerLat().size(); i++) {
+                markerOptions.position(new LatLong(Database.getMarkerLat().get(i), Database.getMarkerLong().get(i)))
+                        .visible(true)
+                        .title("test");
+                Marker marker = new Marker(markerOptions);
+                map.addMarker(marker);
+            }
         }
 
+
         public static void main(String[] args) {
-            Database Database = new Database();
-            Database.execute("SELECT X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE Inventarisnr=100.0", "X_Coord");
-            double X_Coord = Double.parseDouble(Database.getSpecific(0));
-            Database.execute("SELECT X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE Inventarisnr=100.0", "Y_Coord");
-            double Y_Coord = Double.parseDouble(Database.getSpecific(0));
-            // ^ opvragen x & y coordinaten
-            Zino.createMarker(X_Coord, Y_Coord, true, "test");
             launch(args);
         }
     }
 
+//
+//Database.execute("SELECT X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE Inventarisnr=100.0", "X_Coord");
+//        double X_Coord = Double.parseDouble(Database.getSpecific(1));
+//        Database.execute("SELECT X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE Inventarisnr=100.0", "Y_Coord");
+//        double Y_Coord = Double.parseDouble(Database.getSpecific(1));
+//        // ^ opvragen x & y coordinaten
+//        createMarker(X_Coord, Y_Coord, true, "test");
