f4d48ccb4d13b8bf2c26e0ff1d70e1f2152d8e0d
Zinoq <0893504@hr.nl>
2016-04-12 13:18:40 +0200

    the progression is real

--- src/PieChart.java
+++ src/PieChart.java
@@ -5,49 +5,37 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.chart.*;
 import javafx.scene.Group;
-import java.util.ArrayList;
 
 public class Piechart extends Application {
-    String Title;
-    int Width;
-    int Height;
-    String Result1;
-    String Result2;
-    Database Database;
+    Database Database = new Database();
+    double Noord = Double.parseDouble(Database.execute("SELECT COUNT(*) FROM standard.parkeerautomaten WHERE Deelgem=\"Noord\";", "Count(*)").get(0));
+    double Centrum = Double.parseDouble(Database.execute("SELECT COUNT(*) FROM standard.parkeerautomaten WHERE Deelgem=\"Centrum\";", "Count(*)").get(1));
+    double Charlois = Double.parseDouble(Database.execute("SELECT COUNT(*) FROM standard.parkeerautomaten WHERE Deelgem=\"Charlois\";", "Count(*)").get(2));
+    double Delfshaven = Double.parseDouble(Database.execute("SELECT COUNT(*) FROM standard.parkeerautomaten WHERE Deelgem=\"Delshaven\";", "Count(*)").get(3));
+    double Feijenoord = Double.parseDouble(Database.execute("SELECT COUNT(*) FROM standard.parkeerautomaten WHERE Deelgem=\"Feijenoord\";", "Count(*)").get(4));
+    double IJsselmonde =  Double.parseDouble(Database.execute("SELECT COUNT(*) FROM standard.parkeerautomaten WHERE Deelgem=\"Ijsselmonde\";", "Count(*)").get(5));
+    double Kralingen = Double.parseDouble(Database.execute("SELECT COUNT(*) FROM standard.parkeerautomaten WHERE Deelgem=\"Kralingen/Crooswijk\";", "Count(*)").get(6));
+    double PrinsAlexander = Double.parseDouble(Database.execute("SELECT COUNT(*) FROM standard.parkeerautomaten WHERE Deelgem=\"Prins Alexander\";", "Count(*)").get(7));
 
-      public Piechart(String title, int Width, int Height) {
-          this.Title = title;
-          this.Width = Width;
-          this.Height = Height;
-      }
-
-        public String getResult1(String Query1, String Selector1) {
-            Result1 = Database.execute(Query1, Selector1);
-            System.out.println(Result1);
-            return Result1;
-        }
-
-        public String getResult2(String Query2, String Selector2) {
-            Result2 = Database.execute(Query2, Selector2);
-            System.out.println(Result2);
-            return Result2;
-        }
 
         @Override public void start(Stage stage) {
             Scene scene = new Scene(new Group());
-            stage.setTitle(this.Title);
-            stage.setWidth(this.Width);
-            stage.setHeight(this.Height);
+            stage.setTitle("Test");
+            stage.setWidth(800);
+            stage.setHeight(800);
 
             ObservableList<PieChart.Data> pieChartData =
                     FXCollections.observableArrayList(
-                            new PieChart.Data("Grapefruit", 13),
-                            new PieChart.Data("Oranges", 25),
-                            new PieChart.Data("Plums", 10),
-                            new PieChart.Data("Pears", 22),
-                            new PieChart.Data("Apples", 30));
+                            new PieChart.Data("Noord", Noord),
+                            new PieChart.Data("Centrum",Centrum),
+                            new PieChart.Data("Charlois", Charlois),
+                            new PieChart.Data("Delfshaven", Delfshaven),
+                            new PieChart.Data("Feijenoord", Feijenoord),
+                            new PieChart.Data("IJsselmonde",IJsselmonde),
+                            new PieChart.Data("Kralingen/Crooswijk", Kralingen),
+                            new PieChart.Data("Prins Alexander", PrinsAlexander));
             final PieChart chart = new PieChart(pieChartData);
-            chart.setTitle(this.Title);
+            chart.setTitle("Aantal parkeerautomaten per deelgemeente");
 
             ((Group) scene.getRoot()).getChildren().add(chart);
             stage.setScene(scene);
@@ -55,12 +43,6 @@ public class Piechart extends Application {
         }
 
         public static void main(String[] args) {
-            Piechart chart1 = new Piechart("Test", 500, 500);
-            Database Database = new Database();
-            chart1.getResult1("SELECT Inventarisnr FROM standard.parkeerautomaten WHERE Inventarisnr=100.0", "Inventarisnr");
-            chart1.getResult2("SELECT Deelgem FROM standard.parkeerautomaten WHERE Inventarisnr=100.0", "Deelgem");
-
-
-//            launch(args);
+            launch(args);
         }
     }
