876a576197c387bd097910116eb50330ee9eb1cd
AndreHummel <andre.97@hotmail.com>
2016-04-12 11:18:07 +0200

    markers uit database toegevoegd

--- src/Map.java
+++ src/Map.java
@@ -10,10 +10,14 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Map extends Application implements MapComponentInitializedListener{
         GoogleMapView mapView;
         GoogleMap map;
         Database Database;
+        Scene scene;
+
 
         public Map(){
             //Create the JavaFX component and set this as a listener so we know when
@@ -21,16 +25,17 @@ public class Map extends Application implements MapComponentInitializedListener{
             mapView = new GoogleMapView();
             mapView.addMapInializedListener(this);
             Database = new Database();
+            scene = new Scene(mapView,1280,720);
         }
 
         @Override
         public void start(Stage stage) throws Exception {
-            Scene scene = new Scene(mapView);;
             stage.setTitle("Map");
             stage.setScene(scene);
             stage.show();
         }
 
+        // initialize the map
         @Override
         public void mapInitialized() {
             //Set the initial properties of the map.
@@ -52,12 +57,10 @@ public class Map extends Application implements MapComponentInitializedListener{
             //Add a marker to the map.
             MarkerOptions markerOptions = new MarkerOptions();
 
-            for (int i = 0; i < Database.getMarkerLat().size(); i++) {
-                markerOptions.position(new LatLong(Database.getMarkerLat().get(i), Database.getMarkerLong().get(i)))
-                        .visible(true)
-                        .title("test");
-                Marker marker = new Marker(markerOptions);
-                map.addMarker(marker);
+            for (int i = 0; i < 200; i++) {
+                map.addMarker(new Marker(markerOptions.position(new LatLong(Database.getMarkerLong().get(i), Database.getMarkerLat().get(i)))
+                        .visible(Boolean.TRUE)
+                        .title("test")));
             }
         }
 
@@ -67,10 +70,3 @@ public class Map extends Application implements MapComponentInitializedListener{
         }
     }
 
-//
-//Database.execute("SELECT X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE Inventarisnr=100.0", "X_Coord");
-//        double X_Coord = Double.parseDouble(Database.getSpecific(1));
-//        Database.execute("SELECT X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE Inventarisnr=100.0", "Y_Coord");
-//        double Y_Coord = Double.parseDouble(Database.getSpecific(1));
-//        // ^ opvragen x & y coordinaten
-//        createMarker(X_Coord, Y_Coord, true, "test");
