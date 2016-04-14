5de60085cdbc13876029648acc5b36e7e86f0281
Zinoq <0893504@hr.nl>
2016-04-14 10:09:20 +0200

    plaats veranderd

---
+++ assets/parkeerautomaten-fix.sql
@@ -0,0 +1,17 @@
+
+-- Om te fixen
+SET SQL_SAFE_UPDATES = 0;
+
+delete from standard.parkeerautomaten WHERE Inventarisnr=142.0;
+delete FROM standard.parkeerautomaten WHERE Y_Coord > 45;
+delete from standard.parkeerautomaten WHERE X_Coord < 51.7;
+
+
+-- On-commenten om te checken of het is gelukt 
+
+-- SELECT Inventarisnr, X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE X_Coord < 45;
+-- SELECT Inventarisnr, X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE Y_Coord > 45;
+-- SELECT Inventarisnr, X_Coord, Y_Coord FROM standard.parkeerautomaten WHERE X_Coord < 51.7;
+
+-- Als je met alles klaar bent
+-- SET SQL_SAFE_UPDATES = 1;
