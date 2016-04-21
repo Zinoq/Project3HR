3b136a2e7356445a695ebd189fe87c494985f79f
Zinoq <0893504@hr.nl>
2016-04-21 12:03:28 +0200

    added comments and removed unused/unneccesary stuff

--- src/Database.java
+++ src/Database.java
@@ -1,22 +1,15 @@
-
 import java.sql.*;
 import java.util.ArrayList;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.SQLException;
-
 
 public class Database {
     private ArrayList<String> Result = new ArrayList<>();
     private Connection connection;
     private ArrayList<Double> XMLong = new ArrayList<>();
     private ArrayList<Double> YMLat = new ArrayList<>();
-    private ArrayList<Double> XPLong = new ArrayList<>();
-    private ArrayList<Double> YPLat = new ArrayList<>();
 
     public Database() {
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/standard?" + "user=root&password=Kappa123"); //connectie maken met database
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/standard?" + "user=root&password=Kappa123"); //make connection with database hosted at localhost
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -28,14 +21,13 @@ public class Database {
             ResultSet myResultset = myStatement.executeQuery(Query); //met een statement kunnen we sql code runnen, die meegegeven wordt
             while (myResultset.next()) { // eigenlijk een soort for loop door de resultaten
                 Result.clear();
-                Result.add(myResultset.getString(Selector));
+                Result.add(myResultset.getString(Selector)); // add String of resultset to our result list
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return Result;
     }
-
 
     public ArrayList<Double> getMarkerLat() {
         try {
@@ -52,7 +44,7 @@ public class Database {
         }
         //        System.out.println(Results);
         return XMLong;
-    }
+    } // always returns the Lat at the markers table
 
     public ArrayList<Double> getMarkerLong() {
         try {
@@ -69,40 +61,7 @@ public class Database {
         }
         //        System.out.println(Results);
         return YMLat;
-    }
+    } // always returns the Lon at the markers able
 
-    public ArrayList<Double> getParkeerLat() {
-        try {
-            Statement myStatement = connection.createStatement();
-            ResultSet myResultset = myStatement.executeQuery("SELECT X_Coord FROM standard.parkeerautomaten WHERE X_Coord IS NOT NULL");
-
-            // output
-            while (myResultset.next()) { //loop door alle resultaten als er meerdere zijn
-//                System.out.println(myResultset.getDouble("X_Coord"));
-                XPLong.add(myResultset.getDouble("X_coord")); //Alle resultaten worden in een lijst gezet, waar we later mee kunnen werken
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        //        System.out.println(Results);
-        return XPLong;
-    }
-
-    public ArrayList<Double> getParkeerLong() {
-        try {
-            Statement myStatement = connection.createStatement();
-            ResultSet myResultset = myStatement.executeQuery("SELECT Y_Coord FROM standard.parkeerautomaten WHERE Y_Coord IS NOT NULL");
-
-            // output
-            while (myResultset.next()) { //loop door alle resultaten als er meerdere zijn
-//                System.out.println(myResultset.getDouble("Y_Coord"));
-                YPLat.add(myResultset.getDouble("Y_Coord")); //Alle resultaten worden in een lijst gezet, waar we later mee kunnen werken
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        //        System.out.println(Results);
-        return YPLat;
-    }
 
 }
